9c0a3a087d187166ce521cd68f6475d014df9756
James Abel <j@abel.co>
2020-12-06 13:12:38 -0800

    development continues

--- bup/gui/about.py
+++ bup/gui/about.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from PyQt5.QtWidgets import QVBoxLayout, QWidget, QTextEdit
+from PyQt5.QtWidgets import QVBoxLayout, QWidget, QTextBrowser
 
 from bup import __url__, __author_url__
 
@@ -12,12 +12,12 @@ class BupAbout(QWidget):
 
         about_text = Path("LICENSE").read_text()
         about_text += "\n---\n"
-        about_text += f"\nsource code: {__url__}\n"
-        about_text += f"\nauthor's web site: {__author_url__}\n"
+        about_text += f"\nsource code: [{__url__}]({__url__})\n"
+        about_text += f"\nauthor's web site: [{__author_url__}]({__author_url__})\n"
         about_text += "\n---\n"
         about_text += Path("gpl-3.0.md").read_text()
 
-        self.about_text_widget = QTextEdit()
+        self.about_text_widget = QTextBrowser()
+        self.about_text_widget.setOpenExternalLinks(True)
         self.about_text_widget.setMarkdown(about_text)
-        self.about_text_widget.setReadOnly(True)
         self.layout().addWidget(self.about_text_widget)
