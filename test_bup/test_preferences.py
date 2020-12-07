d3c15fa380fc2a0da75b26b7317a999eade2bfb1
James Abel <j@abel.co>
2020-12-07 15:03:28 -0800

    fix issue with immediate restart after start fix issue with no preferences DB causing crash use GUI for errors but set dialog box to ERROR even if verbose is set

--- test_bup/test_preferences.py
+++ test_bup/test_preferences.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-from bup.gui import BupPreferences, PreferencesList
+from bup import BupPreferences, ExclusionPreferences, BackupTypes
 
 from bup import BackupTypes, __author__
 from test_bup import __application_name__
@@ -14,10 +14,12 @@ def test_preferences():
     preferences = BupPreferences(__application_name__, __author__)
     assert preferences.aws_profile == test_aws_profile
 
-    exclusions = PreferencesList(__application_name__, __author__, BackupTypes.S3.name)
+    exclusions = ExclusionPreferences(BackupTypes.S3.name)
+    saved_exclusions = exclusions.get()
     test_list = ["a", "b", "a", "", "qwertyuiop", "c", 1]  # two "a"s, and an int 1
     expected_results = deepcopy(test_list)
     expected_results.pop(0)  # remove 1st "a"
     expected_results[-1] = str(expected_results[-1])  # the input value can be non-string, but it'll be returned as a string
     exclusions.set(test_list)
     assert exclusions.get() == expected_results
+    exclusions.set(saved_exclusions)
--- test_bup/test_preferences_gui.py
+++ test_bup/test_preferences_gui.py
@@ -3,21 +3,15 @@ from pprint import pprint
 from ismain import is_main
 from PyQt5.Qt import QApplication
 
-from bup.gui import BupPreferences, PreferencesWidget
+from bup import BupPreferences, __author__, get_preferences, UITypes
+from bup.gui import PreferencesWidget
 
 
 def test_preferences():
 
     # todo: this currently just tests what's ever in the preferences, which can be nothing - in the future add writing test data
-
-    gui_preferences = BupPreferences()
+    gui_preferences = get_preferences(UITypes.cli)
     pprint(gui_preferences)
-
-    app = QApplication([])  # needed for the preferences widget
-    preferences_widget = PreferencesWidget()
-    pprint(preferences_widget)
-
-    assert gui_preferences.items() == preferences_widget.items()
 
 
 if is_main():
