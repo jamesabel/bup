e6ed1ac61707dd9ba5a5f26ff63e552329b01e06
James Abel <j@abel.co>
2020-12-07 16:42:53 -0800

    fix AWS CLI path for shipped app

--- bup/s3_backup.py
+++ bup/s3_backup.py
@@ -58,54 +58,64 @@ class S3Backup(BupBase):
             else:
                 self.info_out(f"{bucket_name}")
 
-                aws_cli_path = f'"{os.path.abspath(os.path.join("venv", "Scripts", "aws"))}"'
+                # try to find the AWS CLI app
+                aws_cli_paths = [Path("venv", "Scripts", "aws").absolute(),
+                                 Path("Scripts", "aws").absolute()]
+                aws_cli_path = None
+                for path in aws_cli_paths:
+                    if path.exists():
+                        aws_cli_path = path
+                        break
 
-                destination = Path(backup_directory, bucket_name)
-                os.makedirs(destination, exist_ok=True)
-                s3_bucket_path = f"s3://{bucket_name}"
-                # Don't use --delete.  We want to keep 'old' files locally.
-                sync_command_line = [aws_cli_path, "s3", "sync", s3_bucket_path, str(destination.absolute())]
-                if dry_run:
-                    sync_command_line.append("--dryrun")
-                sync_command_line_str = " ".join(sync_command_line)
-                log.info(sync_command_line_str)
-
-                try:
-                    sync_result = subprocess.run(sync_command_line_str, stdout=subprocess.PIPE, shell=True)
-                except FileNotFoundError as e:
-                    self.error_out(f'error executing {" ".join(sync_command_line)} {e}')
-                    return
-
-                for line in sync_result.stdout.decode(decoding).splitlines():
-                    log.info(line.strip())
-
-                # check the results
-                ls_command_line = [aws_cli_path, "s3", "ls", "--summarize", "--recursive", s3_bucket_path]
-                ls_command_line_str = " ".join(ls_command_line)
-                log.info(ls_command_line_str)
-                ls_result = subprocess.run(ls_command_line_str, stdout=subprocess.PIPE, shell=True)
-                count += 1
-                ls_stdout = "".join([c for c in ls_result.stdout.decode(decoding) if c not in " \r\n"])  # remove all whitespace
-                ls_parsed = ls_re.search(ls_stdout)
-                if ls_parsed is None:
-                    log.error(f"parse error:\n{ls_command_line_str=}\n{ls_stdout=}")
+                if aws_cli_path is None:
+                    log.error(f"AWS CLI executable not found ({aws_cli_paths=})")
                 else:
-                    s3_object_count = int(ls_parsed.group(1))
-                    s3_total_size = int(ls_parsed.group(2))
-                    local_size, local_count = get_dir_size(destination)
+                    destination = Path(backup_directory, bucket_name)
+                    os.makedirs(destination, exist_ok=True)
+                    s3_bucket_path = f"s3://{bucket_name}"
+                    # Don't use --delete.  We want to keep 'old' files locally.
+                    sync_command_line = [str(aws_cli_path), "s3", "sync", s3_bucket_path, str(destination.absolute())]
+                    if dry_run:
+                        sync_command_line.append("--dryrun")
+                    sync_command_line_str = " ".join(sync_command_line)
+                    log.info(sync_command_line_str)
 
-                    # rough check that the sync worked
-                    if s3_total_size > local_size:
-                        # we're missing files
-                        message = "not all files backed up"
-                        output_routine = self.error_out
-                    elif s3_total_size != local_size:
-                        # Compare size, not number of files, since aws s3 sync does not copy files of zero size.
-                        message = "mismatch"
-                        output_routine = self.warning_out
+                    try:
+                        sync_result = subprocess.run(sync_command_line_str, stdout=subprocess.PIPE, shell=True)
+                    except FileNotFoundError as e:
+                        self.error_out(f'error executing {" ".join(sync_command_line)} {e}')
+                        return
+
+                    for line in sync_result.stdout.decode(decoding).splitlines():
+                        log.info(line.strip())
+
+                    # check the results
+                    ls_command_line = [aws_cli_path, "s3", "ls", "--summarize", "--recursive", s3_bucket_path]
+                    ls_command_line_str = " ".join(ls_command_line)
+                    log.info(ls_command_line_str)
+                    ls_result = subprocess.run(ls_command_line_str, stdout=subprocess.PIPE, shell=True)
+                    count += 1
+                    ls_stdout = "".join([c for c in ls_result.stdout.decode(decoding) if c not in " \r\n"])  # remove all whitespace
+                    ls_parsed = ls_re.search(ls_stdout)
+                    if ls_parsed is None:
+                        log.error(f"parse error:\n{ls_command_line_str=}\n{ls_stdout=}")
                     else:
-                        message = "match"
-                        output_routine = log.info
-                    output_routine(f"{bucket_name} : {message} (s3_count={s3_object_count}, local_count={local_count}; s3_total_size={s3_total_size}, local_size={local_size})")
+                        s3_object_count = int(ls_parsed.group(1))
+                        s3_total_size = int(ls_parsed.group(2))
+                        local_size, local_count = get_dir_size(destination)
+
+                        # rough check that the sync worked
+                        if s3_total_size > local_size:
+                            # we're missing files
+                            message = "not all files backed up"
+                            output_routine = self.error_out
+                        elif s3_total_size != local_size:
+                            # Compare size, not number of files, since aws s3 sync does not copy files of zero size.
+                            message = "mismatch"
+                            output_routine = self.warning_out
+                        else:
+                            message = "match"
+                            output_routine = log.info
+                        output_routine(f"{bucket_name} : {message} (s3_count={s3_object_count}, local_count={local_count}; s3_total_size={s3_total_size}, local_size={local_size})")
 
         self.info_out(f"{len(buckets)} buckets, {count} backed up, {len(exclusions)} excluded")
