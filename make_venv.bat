228ee80bff3118d2da3d80cf641287a6bbf5610f
JamesAbel <j@abel.co>
2025-03-02 09:23:51 -0800

    Python 3.13

--- make_venv.bat
+++ make_venv.bat
@@ -1,6 +1,6 @@
 mkdir temp
 rmdir /S /Q venv
-"C:\Program Files\Python311\python.exe" -m venv --clear venv
+"C:\Program Files\Python313\python.exe" -m venv --clear venv
 venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -U setuptools
 venv\Scripts\python -m pip install -U -r requirements-dev.txt
